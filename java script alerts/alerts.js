4f2705f46d7ff91fb3a508ba95c49f6de629b874
Muhammad Ahmed Siddiqui <154745545+mahmedsiddiqui@users.noreply.github.com>
2024-02-29 10:51:07 +0500

    Add files via upload

---
+++ java script alerts/alerts.js
@@ -0,0 +1,8 @@
+// there is the format of writing alert desfined below
+
+// The alert() method displays an alert box with a message and an OK button.
+
+// The alert() method is used when you want information to come through to the user.
+
+alert("Your Name Is Ahmed")
+
